907e7d2261171f33b4c52bb62f7a0138cb00d5c2
Robert Kossessa <github@kosro.de>
2024-12-23 01:52:23 +0100

    ADD: Sidebar + dark mode toggle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,38 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
+    import FontList from "$lib/components/font-list.svelte"
+    import { invoke } from "@tauri-apps/api/core"
 
-  let installed_families: FontFamily[] = [];
+    import DarkModeToggle from "$lib/components/dark-mode-toggle.svelte"
 
-  invoke<Font[]>("list_fonts").then((fonts) => {
-    fonts.forEach((font) => {
-      const family = installed_families.find(
-        (f) => f.family_name === font.family_name
-      );
-      if (family) {
-        family.fonts.push(font);
-      } else {
-        installed_families.push({
-          family_name: font.family_name,
-          fonts: [font],
-        });
-      }
-    });
-  });
+    import * as Sidebar from "$lib/components/ui/sidebar/index.js"
+    import AppSidebar from "$lib/components/app-sidebar.svelte"
+
+    let installed_families: FontFamily[] = []
+
+    invoke<Font[]>("list_fonts").then((fonts) => {
+        fonts.forEach((font) => {
+            const family = installed_families.find(
+                (f) => f.family_name === font.family_name
+            )
+            if (family) {
+                family.fonts.push(font)
+            } else {
+                installed_families.push({
+                    family_name: font.family_name,
+                    fonts: [font],
+                })
+            }
+        })
+    })
 </script>
 
-<main>
-  <p>Hello, World!</p>
-</main>
+<Sidebar.Provider>
+    <AppSidebar />
+    <main class="h-screen w-full flex flex-col">
+        <div class="flex items-center justify-between p-2 border-b">
+            <Sidebar.Trigger />
+            <DarkModeToggle />
+        </div>
+        <FontList />
+    </main>
+</Sidebar.Provider>
